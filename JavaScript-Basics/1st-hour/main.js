0ca1887124b190ff1f02305c5727731b41c7bc3a
Imani Noel <826962@KH-STU-MBA-C1MV7GMJJ1WK-826962.local>
2021-12-06 14:12:02 -0600

    adding in our 1st-hour lessons

---
+++ JavaScript-Basics/1st-hour/main.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.btn').forEach((item)=>{
+    item.addEventListener('click',()=> {
+        alert('sydney is the greatest');
+    });
+});
+
+alert('Hello Elvis');
+
+document.write({name: 'hello Elvis',
+email: 'elvis@elvis.com'
+});
+
+console.log({name: 'Hello Elvis from console'});
